472cb5e1413bc34f6ef5187fb7251e1278a86dbc
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-12-10 19:57:44 +1100

    feat: french translation for paris district

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export * from "./chess";
-export * from "./map";
+export * from "./locationInfos";
 export * from "./englishToFrench";
 export * from "./frenchToEnglish";
---
+++ src/constants/locationInfos.ts
@@ -0,0 +1,239 @@
+import { Locations, type LocationInfo } from "@/types";
+
+export const locationInfos: { [key in Locations]: LocationInfo | undefined } = {
+  [Locations.ParisDistrict]: {
+    nameEnglish: "Paris District",
+    nameFrench: "Quartier de Parisien",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Un cœur autrefois opulent",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Autrefois un cœur d'opulence dans le monde, aujourd'hui détruit par les luttes politiques intestines.",
+    influenceAvailable: 20,
+  },
+
+  [Locations.AimensDistrict]: {
+    nameEnglish: "Aimens District",
+    nameFrench: "Aimens District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.RennesDistrictWest]: {
+    nameEnglish: "Rennes District (West)",
+    nameFrench: "Rennes District (West)",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.RennesDistrictEast]: {
+    nameEnglish: "Rennes District (East)",
+    nameFrench: "Rennes District (East)",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.PotiersDistrict]: {
+    nameEnglish: "Potiers District",
+    nameFrench: "Potiers District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.OrleansDistrict]: {
+    nameEnglish: "Orleans District",
+    nameFrench: "Orleans District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 45,
+    districtHectares: 108,
+    subtitleEnglish: "Blues capital of the old world",
+    subtitleFrench: "Blues capital of the old world",
+    descriptionEnglish:
+      "Famous for it's swamps and guitars, Orleans is famous as the setting for Disney's \"The Princess and the Frog (2032)\".",
+    descriptionFrench:
+      "Famous for it's swamps and guitars, Orleans is famous as the setting for Disney's \"The Princess and the Frog (2032)\".",
+    influenceAvailable: 50,
+  },
+
+  [Locations.BordeauxDistrict]: {
+    nameEnglish: "Bordeaux District",
+    nameFrench: "Bordeaux District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.TolouseDistrict]: {
+    nameEnglish: "Tolouse District",
+    nameFrench: "Tolouse District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.MarsielleDistrict]: {
+    nameEnglish: "Marsielle District",
+    nameFrench: "Marsielle District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.ValenceDistrict]: {
+    nameEnglish: "Valence District",
+    nameFrench: "Valence District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.LyonDistrict]: {
+    nameEnglish: "Lyon District",
+    nameFrench: "Lyon District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.DijonDistrict]: {
+    nameEnglish: "Dijon District",
+    nameFrench: "Dijon District",
+    cityCoordinates: {
+      latitude: 80,
+      longitude: 50,
+    },
+    cityHectares: 59,
+    districtHectares: 160,
+    subtitleEnglish: "Mustard land",
+    subtitleFrench: "Mustard land",
+    descriptionEnglish:
+      "Dijon is known for two things. One is the renowned Hermione ski scene, re-enacted by thousands of fans every April. The other is mustard production. No other district in the world produces mustard by waterfall. In fact, no other district in the world produces mustard, full stop.",
+    descriptionFrench:
+      "Dijon is known for two things. One is the renowned Hermione ski scene, re-enacted by thousands of fans every April. The other is mustard production. No other district in the world produces mustard by waterfall. In fact, no other district in the world produces mustard, full stop.",
+    influenceAvailable: 80,
+  },
+
+  [Locations.ReimsDistrict]: {
+    nameEnglish: "Reims District",
+    nameFrench: "Reims District",
+    cityCoordinates: {
+      latitude: 23,
+      longitude: 45,
+    },
+    cityHectares: 58,
+    districtHectares: 108,
+    subtitleEnglish: "Once opulent heart",
+    subtitleFrench: "Once opulent heart",
+    descriptionEnglish:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    descriptionFrench:
+      "Once a heart of opulence in the world, now destroyed by political infighting.",
+    influenceAvailable: 55,
+  },
+
+  [Locations.None]: undefined,
+};
