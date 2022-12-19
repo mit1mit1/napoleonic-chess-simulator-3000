635dd9e3d44e30d3a00c8b078febc2bb1fac68a8
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-12-20 02:10:29 +1100

    feat: more translations

--- src/constants/frenchToEnglish.ts
+++ src/constants/frenchToEnglish.ts
@@ -3,86 +3,150 @@ import type { Dictionary } from "@/types";
 export const frenchToEnglish: Dictionary = {
   à: {
     englishDefinition: "",
-    translations: ["in", "to", "at"]
+    translations: ["in", "to", "at"],
   },
   ai: {
     englishDefinition: "",
-    translations: ["have"]
+    translations: ["have"],
   },
   ce: {
     englishDefinition: "",
-    translations: ["this", "that"]
+    translations: ["this", "that"],
+  },
+  ["c'est"]: {
+    englishDefinition: "",
+    translations: ["it is", "is", "this is"],
   },
   connaissez: {
     englishDefinition: "",
-    translations: ["know", "know about"]
+    translations: ["know", "know about"],
   },
   ["connaissez-vous"]: {
     englishDefinition: "",
-    translations: ["do you know", "are you familiar with"]
+    translations: ["do you know", "are you familiar with"],
+  },
+  coordonnées: {
+    englishDefinition: "",
+    translations: ["contact details", "co-ordinates"],
+  },
+  de: {
+    englishDefinition: "",
+    translations: ["from", "of", "at"],
+  },
+  ["défoncer"]: {
+    englishDefinition: "",
+    translations: ["smash", "bash", "break"],
   },
   depuis: {
     englishDefinition: "",
-    translations: ["since", "from"]
+    translations: ["since", "from"],
+  },
+  des: {
+    englishDefinition: "",
+    translations: ["of", "from", "of the"],
+  },
+  du: {
+    englishDefinition: "",
+    translations: ["from", "of", "of the", "at"],
+  },
+  drogue: {
+    englishDefinition: "",
+    translations: ["drug", "drugs"],
   },
   français: {
     englishDefinition: "",
-    translations: ["French"]
+    translations: ["French"],
   },
   hum: {
     englishDefinition: "expression of uncertainty or hesitence",
-    translations: ["um"]
+    translations: ["um"],
+  },
+  idéal: {
+    englishDefinition: "",
+    translations: ["ideal", "best", "perfect"],
   },
   j: {
     englishDefinition: "",
-    translations: ["I"]
+    translations: ["I"],
   },
   ["j'ai"]: {
     englishDefinition: "",
-    translations: ["I have"]
+    translations: ["I have"],
+  },
+  la: {
+    englishDefinition: "",
+    translations: ["the"],
   },
   ["l'anglais"]: {
     englishDefinition: "",
-    translations: ["English", "the English language"]
+    translations: ["English", "the English language"],
+  },
+  ["l'endroit"]: {
+    englishDefinition: "",
+    translations: ["the place", "the location"],
   },
   ["l'époque"]: {
     englishDefinition: "",
-    translations: ["the time", "at the time", "the era"]
+    translations: ["the time", "at the time", "the era"],
   },
   le: {
     englishDefinition: "",
-    translations: ["the"]
+    translations: ["the"],
   },
   mot: {
     englishDefinition: "",
-    translations: ["word", "note"]
+    translations: ["word", "note"],
   },
   ["n'existait"]: {
     englishDefinition: "",
-    translations: ["didn't exist"]
+    translations: ["didn't exist"],
   },
   oh: {
     englishDefinition: "interjection of surprise or realisation",
-    translations: ["oh"]
+    translations: ["oh"],
   },
   oublié: {
     englishDefinition: "",
-    translations: ["forgotten", "forgot"]
+    translations: ["forgotten", "forgot"],
   },
   pas: {
     englishDefinition: "",
-    translations: ["no", "not", "step"]
+    translations: ["no", "not", "step"],
+  },
+  pays: {
+    englishDefinition: "",
+    translations: ["country", "countries"],
   },
   quand: {
     englishDefinition: "",
-    translations: ["when", "at"]
+    translations: ["when", "at"],
+  },
+  quartier: {
+    englishDefinition: "",
+    translations: ["district", "neighbourhood"],
+  },
+  si: {
+    englishDefinition: "",
+    translations: ["if", "so"],
   },
   sire: {
     englishDefinition: "respectful title to address a monarch",
-    translations: ["sire"]
+    translations: ["sire"],
+  },
+  taille: {
+    englishDefinition: "",
+    translations: ["size", "height"],
+  },
+  ville: {
+    englishDefinition: "",
+    translations: ["city", "town"],
   },
   vous: {
     englishDefinition: "",
-    translations: ["you", "you at"]
-  }
+    translations: ["you", "you at"],
+  },
+  voulez: {
+    englishDefinition: "",
+    translations: ["want", "want to"],
+  },
 };
