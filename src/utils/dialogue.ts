e0aed792ace994b0e7b6f881815d3f35bc1dd8b3
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-12-22 13:32:15 +1100

    feat: random dialogue

--- src/utils/dialogue.ts
+++ src/utils/dialogue.ts
@@ -1,8 +1,12 @@
 export const generateSassyFrenchMessage = (prompt: string) => {
+  const random = Math.random();
   if (prompt === "win") {
     return "Bien joué, vous avez battu un morceau de silicium conçu par un simplet";
   }
   if (prompt === "loss") {
+    if (random < 0.5) {
+      return "Kindergarten homme?"
+    }
     return "Dommage, si triste";
   }
   if (prompt === "tie") {
